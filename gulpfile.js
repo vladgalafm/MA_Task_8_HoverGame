cc4097a4b747d832d3fd94e657743ed91c5bf6a3
Vladyslav Halushkin <v.halushkin@gmail.com>
2018-11-10 16:23:14 +0200

    10.11.2018 16:23

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,27 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var rename = require("gulp-rename");
 var browserSync = require("browser-sync");
 
-gulp.task("sass", function(){
-  gulp.src('app/scss/style.scss')
+gulp.task('sass', function(){
+  gulp.src('app/scss/main.scss')
     .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(rename('style.css'))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task("watch",["sass", "browser"], function(){
-  gulp.watch("app/scss/*.scss", ['sass'])
+gulp.task('watch', ['sass', 'browser'], function(){
+  gulp.watch('app/scss/*.scss', ['sass'])
 });
 
-gulp.task("browser", function(){
+gulp.task('browser', function(){
   browserSync({
-    server: {baseDir:"app"},
-    notify: false})
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  })
 });
 
 gulp.task('default', ['watch']);
